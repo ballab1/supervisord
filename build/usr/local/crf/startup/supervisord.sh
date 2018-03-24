2ddc8286b8ed0cec4900df0023ce4834d431714c
Ballantyne, Bob <bob.ballantyne@emc.com>
2018-03-23 20:02:18 -0400

    initial commit

--- build/usr/local/crf/startup/supervisord.sh
+++ build/usr/local/crf/startup/supervisord.sh
@@ -4,5 +4,7 @@ if [ -e /etc/supervisord.conf ]; then
     sed -i \
         -e "s|^username=.*$|username=${SUPERVISORD_USER}|" \
         -e "s|^password=.*$|password=${SUPERVISORD_PASS}|" \
+        -e "s|supervisord:9001|${HOSTNAME}:9001|g" \
            "/etc/supervisord.conf"
+    echo "supervisord available on http://${HOSTNAME}:9001"
 fi
